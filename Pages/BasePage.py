c0274bca39fbf3134bf1382ea0597a274e99ca14
ethanmcf <ethan_mcfarland@outlook.com>
2025-03-21 12:15:52 -0400

    Fixed imports

--- Pages/BasePage.py
+++ Pages/BasePage.py
@@ -1,9 +1,6 @@
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys 
 from selenium.common.exceptions import TimeoutException
-import time
 
 class BasePage:
     """Base page for pages to automate. Provides simple and common functions"""
--- Pages/LoginPage.py
+++ Pages/LoginPage.py
@@ -1,8 +1,5 @@
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support import expected_conditions as EC
 from Pages.BasePage import BasePage
-from selenium.webdriver.support.ui import WebDriverWait
 import time
 
 class LoginPage(BasePage):
