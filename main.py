8767a4e726eeb7b62c1b923ed2d238baf4369d87
Rayce Ramsay <ramsayrayce@gmail.com>
2025-03-29 11:12:01 -0400

    Add basketball drop-in URL

--- main.py
+++ main.py
@@ -18,7 +18,8 @@ BYPASS_CODES_URL = "https://bypass.utormfa.utoronto.ca/index.php"
 ACTIVITY_URLS = {
     "golf": "https://recreation.utoronto.ca/program/GetProgramDetails?courseId=5904837f-6aa4-4707-bcfb-2ece4049bae0",
     "soccer": "https://recreation.utoronto.ca/program/GetProgramDetails?courseId=c762f797-ce00-465f-9acd-52dc42f7eb42",
-    "hockey": "https://recreation.utoronto.ca/Program/GetProgramDetails?courseId=dcd5a035-731e-416b-a546-5f808404a3dc"
+    "hockey": "https://recreation.utoronto.ca/Program/GetProgramDetails?courseId=dcd5a035-731e-416b-a546-5f808404a3dc",
+    "basketball": "https://recreation.utoronto.ca/Program/GetProgramDetails?courseId=56e18068-7b50-45df-8854-1921ad1bee80"
 }
 AGENTS = [
     "Mozilla/5.0 (Windows NT 4.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36",
