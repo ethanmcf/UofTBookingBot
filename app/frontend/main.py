8e457f861f8f01f47b4d049b4c5d25f89607d45d
ethanmcf <ethan_mcfarland@outlook.com>
2025-12-26 15:07:55 -0500

    Created landing page

--- app/frontend/main.py
+++ app/frontend/main.py
@@ -1,5 +1,5 @@
 import sys
-from components.header import Header
+from app.frontend.pages.landing_page import LandingPage
 from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout
 
 def main():
@@ -7,12 +7,12 @@ def main():
     
     main_win = QWidget()
     main_win.setStyleSheet("background-color: white;")
-    main_win.resize(900, 300)
+    main_win.setFixedWidth(1200)
+    main_win.setFixedHeight(800) 
     
     v_layout = QVBoxLayout(main_win)
     v_layout.setContentsMargins(0,0,0,0)
-    v_layout.addWidget(Header())
-    v_layout.addStretch()
+    v_layout.addWidget(LandingPage(), 1)
     
     main_win.show()
     sys.exit(app.exec())
