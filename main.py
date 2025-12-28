3ffdc904a0b633079ff8f6a30a81de93602d0a79
ethanmcf <ethan_mcfarland@outlook.com>
2025-12-28 00:24:58 -0500

    Created main front end run file

---
+++ main.py
@@ -0,0 +1,17 @@
+import sys
+from PyQt6.QtWidgets import QApplication
+from app.frontend.app import BookingApp 
+
+def main():
+    # Create the application instance
+    qt_app = QApplication(sys.argv)
+
+    # Create and show the main window
+    window = BookingApp()
+    window.show()
+
+    # Run the event loop
+    sys.exit(qt_app.exec())
+
+if __name__ == "__main__":
+    main()
