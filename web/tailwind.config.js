f77989ab0ca0ed6463aab9ea933224ba6d6f4571
ethanmcf <ethan_mcfarland@outlook.com>
2026-01-02 15:18:29 -0500

    Config updates

---
+++ web/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        tBlack: "#10141E",
+        primary: "#41A3EB",
+      },
+    },
+  },
+  plugins: [],
+};
